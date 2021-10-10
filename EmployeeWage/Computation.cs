9b7d06924107714ef4cdb9a515894c4479866ca2
sonu1239 <ss316742@gmail.com>
2021-10-10 19:53:16 +0530

    Calculate wages till a condition of total working hours

--- EmployeeWage/Computation.cs
+++ EmployeeWage/Computation.cs
@@ -13,17 +13,18 @@ namespace EmployeeWage72
         public const int IS_FULL_TIME = 1;
         public const int IS_PART_TIME = 2;
         public const int EMP_RET_PR_HR = 50;
-        public const int NUB_OF_WORKING_DAYS =20;
-
+        public const int MAX_WORKING_DAYS =20;
+        public const int MAX_WORKING_HRS = 100;
         static void Main(string[] args)
         {
             //local ver
             int empHrs = 0;
             int empWage = 0;
             int totalEmpWage = 0;
-            int day = 0; 
+            int day = 1;
+            int totalHrs = 0;
             Random random = new Random();
-            for (day = 1; day <= NUB_OF_WORKING_DAYS; day++)
+            while (day <= MAX_WORKING_DAYS&& totalHrs <= MAX_WORKING_HRS)
             {
                 //using next() methad to genret rondom input out 0,1
                 int empInput = random.Next(0, 3);
@@ -44,8 +45,11 @@ namespace EmployeeWage72
                 // formula for calculating emp wage
                 empWage = EMP_RET_PR_HR * empHrs;
                 totalEmpWage += empWage;
+               Console.WriteLine("Daily employee wage for {0} days: {1}, ", day, empWage);
+                day++;
+                totalHrs += empHrs; 
             }
-                Console.WriteLine("Tolat Employee Wage for {0} days :{1} ",day-1,totalEmpWage);
+                Console.WriteLine("Tolat Employee Wage for {0} days :{1} max warking hrs:",day-1,totalEmpWage,totalHrs);
                 Console.ReadLine();
             
         }
