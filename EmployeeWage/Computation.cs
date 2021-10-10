90243da603d3e6aaac3af8ea4bebd572c3fc4149
sonu1239 <ss316742@gmail.com>
2021-10-10 17:07:00 +0530

    part Time Employee Wage

--- EmployeeWage/Computation.cs
+++ EmployeeWage/Computation.cs
@@ -6,13 +6,13 @@ using System.Threading.Tasks;
 
 namespace EmployeeWage72
 {
-    
+    //part timr employee wage and full time employee wage
     class Computation
     { 
         //constant
         public const int IS_FullTime = 1;
         public const int EMP_RET_PR_HR = 50;
-
+        public const int IS_PART_TIME = 2;
         static void Main(string[] args)
         {
             //local ver
@@ -21,15 +21,20 @@ namespace EmployeeWage72
             Random random = new Random();
 
             //using next() methad to genret rondom input out 0,1
-            int empInput = random.Next(0, 2);
+            int empInput = random.Next(0, 3);
             //if_ese programing constact
             if (IS_FullTime == empInput)
             {
-                Console.WriteLine("Employee is prsent");
-                
+                Console.WriteLine("Full time Employee is prsent");
                 empHrs = 8;
             }
-            else
+            else if (IS_PART_TIME == empInput)
+            {
+                Console.WriteLine("part time employee is prsent");
+                empHrs = 4;
+            } 
+
+              else
             {
                 Console.WriteLine("Employee is absent ");
                 empHrs = 0;
