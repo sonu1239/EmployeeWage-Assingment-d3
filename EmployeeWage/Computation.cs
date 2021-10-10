1923671ab174db27b4057545f259851700ad9ae3
sonu1239 <ss316742@gmail.com>
2021-10-10 19:05:05 +0530

    Calculating Wages

--- EmployeeWage/Computation.cs
+++ EmployeeWage/Computation.cs
@@ -13,35 +13,41 @@ namespace EmployeeWage72
         public const int IS_FULL_TIME = 1;
         public const int IS_PART_TIME = 2;
         public const int EMP_RET_PR_HR = 50;
-        
-        
+        public const int NUB_OF_WORKING_DAYS =20;
+
         static void Main(string[] args)
         {
             //local ver
             int empHrs = 0;
             int empWage = 0;
+            int totalEmpWage = 0;
+            int day = 0; 
             Random random = new Random();
-
-            //using next() methad to genret rondom input out 0,1
-            int empInput = random.Next(0, 3);
-            //switch cash  programing constact
-            switch (empInput)
+            for (day = 1; day <= NUB_OF_WORKING_DAYS; day++)
             {
-                case IS_FULL_TIME:
-                    empHrs = 8;
-                    break;
-                case IS_PART_TIME:
-                    empHrs = 4;
-                    break;
+                //using next() methad to genret rondom input out 0,1
+                int empInput = random.Next(0, 3);
+                //switch cash  programing constact
+                switch (empInput)
+                {
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
 
-                default:
-                    empHrs = 0;
-                    break;
-            } 
-             // formula for calculating emp wage
-            empWage = EMP_RET_PR_HR * empHrs;
-               Console.WriteLine("Employee Wage {0}" , empWage);
-               Console.ReadLine();
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                // formula for calculating emp wage
+                empWage = EMP_RET_PR_HR * empHrs;
+                totalEmpWage += empWage;
+            }
+                Console.WriteLine("Tolat Employee Wage for {0} days :{1} ",day-1,totalEmpWage);
+                Console.ReadLine();
+            
         }
     }
 }
