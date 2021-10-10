60a98aefc7b9e6654e55260839beb4bfda59c940
sonu1239 <ss316742@gmail.com>
2021-10-10 15:31:58 +0530

    calculating Employee Wage

--- EmployeeWage/Computation.cs
+++ EmployeeWage/Computation.cs
@@ -6,26 +6,37 @@ using System.Threading.Tasks;
 
 namespace EmployeeWage72
 {
+    
     class Computation
-    {
+    { 
+        //constant
+        public const int IS_FullTime = 1;
+        public const int EMP_RET_PR_HR = 50;
+
         static void Main(string[] args)
         {
-            //constant
-            int IS_PRSENT = 1;
+            //local ver
+            int empHrs = 0;
+            int empWage = 0;
             Random random = new Random();
+
             //using next() methad to genret rondom input out 0,1
             int empInput = random.Next(0, 2);
             //if_ese programing constact
-            if (IS_PRSENT == empInput)
+            if (IS_FullTime == empInput)
             {
                 Console.WriteLine("Employee is prsent");
-                Console.ReadLine();
+                
+                empHrs = 8;
             }
             else
             {
                 Console.WriteLine("Employee is absent ");
-                Console.ReadLine();
+                empHrs = 0;
             }
+            empWage = EMP_RET_PR_HR * empHrs;
+               Console.WriteLine("Employee Wage {0}" , empWage);
+               Console.ReadLine();
         }
     }
 }
