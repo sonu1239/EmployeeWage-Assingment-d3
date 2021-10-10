4e8e3c1d4bb8c0f95d479a68baf065eeb57185d8
sonu1239 <ss316742@gmail.com>
2021-10-10 13:40:20 +0530

    :Check Employee is prsent or Absent. `#

--- EmployeeWage/Computation.cs
+++ EmployeeWage/Computation.cs
@@ -10,8 +10,22 @@ namespace EmployeeWage72
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Wellcome to EmployeeWage Computation Progam");
-            Console.ReadLine();
+            //constant
+            int IS_PRSENT = 1;
+            Random random = new Random();
+            //using next() methad to genret rondom input out 0,1
+            int empInput = random.Next(0, 2);
+            //if_ese programing constact
+            if (IS_PRSENT == empInput)
+            {
+                Console.WriteLine("Employee is prsent");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent ");
+                Console.ReadLine();
+            }
         }
     }
 }
